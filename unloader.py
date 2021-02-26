7ffa85e2a53e68505dd8ad823ee7a27464496358
Andallfor <lwang24@students.gilman.edu>
2021-02-25 21:09:42 -0500

    some test values, might go with this idk

--- unloader.py
+++ unloader.py
@@ -28,16 +28,16 @@ class ai():
 
         self.model = tf.keras.models.Sequential([
             tf.keras.layers.Conv2D(
-                64, (3, 3), activation="relu", input_shape=(28, 28, 1)),
+                80, (3, 3), activation="relu", input_shape=(28, 28, 1)),
 
             tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
                 
             tf.keras.layers.Flatten(),
 
             tf.keras.layers.Dense(160, activation="relu"),
-            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.Dropout(0.25),
             tf.keras.layers.Dense(160, activation="relu"),
-            tf.keras.layers.Dropout(0.1),
+            tf.keras.layers.Dropout(0.25),
 
             tf.keras.layers.Dense(47, activation="softmax")
         ])
@@ -47,5 +47,5 @@ class ai():
             loss="categorical_crossentropy",
             metrics=["accuracy"])
 
-        self.model.fit(self.trainImages, self.traingLabels, epochs=10)
+        self.model.fit(self.trainImages, self.traingLabels, epochs=7)
         self.model.evaluate(self.testImages,  self.testLabels, verbose=2)
