a9c5b2a95e19948fe19cbc10bbdb73db9a7b2c12
Andallfor <lwang24@students.gilman.edu>
2021-02-27 21:26:22 -0500

    process images, ai broken/not inputing images correctly

--- main.py
+++ main.py
@@ -4,11 +4,30 @@ import scipy as s
 import sys
 import os
 import unloader
+import numpy as np
+
+labelKey = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'z', 'y', 'z',]
 
 def main():
+    if (len(sys.argv) != 2):
+        sys.exit("Incorrect Usage.\nUsage: main.py fileName")
+    if not os.path.exists(sys.argv[1]):
+        raise ReferenceError("File given not found")
     u = unloader.ai()
     ai = u.model
     print("loaded ai")
+
+    sizeX, sizeY, imgs = u.unloadImg(sys.argv[1])
+
+    for x in range(sizeX):
+        line = ""
+        for y in range(sizeY):
+            prediction = ai.predict(np.reshape(imgs[x][y], (1, 28, 28, 1))).argmax()
+            if (prediction is not None):
+                line += labelKey[prediction]
+            else:
+                line += " "
+        print(line)
 
 if __name__ == "__main__":
     main()
--- unloader.py
+++ unloader.py
@@ -6,6 +6,7 @@ import sys
 import numpy as np
 import emnist
 import os
+import image
 
 class ai():
     def __init__(self):
@@ -22,12 +23,16 @@ class ai():
         self.trainImages, self.traingLabels = emnist.extract_training_samples('balanced')
         self.testImages, self.testLabels = emnist.extract_test_samples('balanced')
 
+        print(self.trainImages.shape)
+
         self.trainImages, self.testImages = self.trainImages / 255.0, self.testImages / 255.0
+        print(self.trainImages.shape)
         self.traingLabels = tf.keras.utils.to_categorical(self.traingLabels)
         self.testLabels = tf.keras.utils.to_categorical(self.testLabels)
         
         self.trainImages = self.trainImages.reshape(
             self.trainImages.shape[0], self.trainImages.shape[1], self.trainImages.shape[2], 1)
+        print(self.trainImages.shape)
         self.testImages = self.testImages.reshape(
             self.testImages.shape[0], self.testImages.shape[1], self.testImages.shape[2], 1)
 
@@ -56,3 +61,28 @@ class ai():
         self.model.evaluate(self.testImages,  self.testLabels, verbose=2)
 
         self.model.save(os.path.dirname(os.path.abspath(__file__)))
+    
+    def unloadImg(self, path):
+        '''
+        returns sizeX, sizeY, [x][y]
+
+        [x][y] gives (28, 28, 1)
+        '''
+        # load img in black and white
+        img = c.imread(path, c.IMREAD_GRAYSCALE)
+
+        # blur
+        img = c.GaussianBlur(img, (5, 5), 0)
+
+        # get size, increments of 28 (convert to int to floor it)
+        sizeX = int(img.shape[0] / 28)
+        sizeY = int(img.shape[1] / 28)
+
+        returnArray = [[] for i in range(sizeX)]
+
+        for x in range(0, sizeX * 27, 28):
+            for y in range(0, sizeY * 27, 28):
+                s = img[x : x + 28, y : y + 28]
+                returnArray[int(x/28)].append(s)
+
+        return sizeX, sizeY, returnArray
